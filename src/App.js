6698cfc2deff8a55af3d55633edf21abdee50685
Bart Hendrix <info@barthendrix.com>
2019-09-02 18:57:53 +0200

    create-react-setup after trying all sorts of simpler alternatives

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="main">
+        <h1>A simple react accordion</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
