c03071463b787c3beb2fa55345b2639deefe103b
Bart Hendrix <info@barthendrix.com>
2019-09-03 22:07:46 +0200

    Basic behaviour and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Accordion from './Accordion';
+
 import './css/style.css';
+import { planets } from './Planets';
 
 function App() {
+
+
   return (
     <div className="App">
       <main className="main">
         <h1>A simple react accordion</h1>
+        <Accordion data={planets} showBellow={1} slideDuration={900} />
       </main>
     </div>
   );
---
+++ src/Planets.js
@@ -0,0 +1,50 @@
+export const planets = [
+	{
+		"id": 1,
+		"title": "Mercury",
+		"text": "Mercury is the closest planet to the Sun and on average, all seven other planets. The smallest planet in the Solar System, Mercury has no natural satellites. Besides impact craters, its only known geological features are lobed ridges or rupes that were probably produced by a period of contraction early in its history. Mercury's very tenuous atmosphere consists of atoms blasted off its surface by the solar wind.",
+		"image": "https://upload.wikimedia.org/wikipedia/commons/d/d9/Mercury_in_color_-_Prockter07-edit1.jpg"
+	},
+	{
+		"id": 2,
+		"title": "Venus",
+		"text": "Venus is close in size to Earth and, like Earth, has a thick silicate mantle around an iron core, a substantial atmosphere, and evidence of internal geological activity. It is much drier than Earth, and its atmosphere is ninety times as dense. Venus has no natural satellites. It is the hottest planet, with surface temperatures over 400 °C (752 °F), most likely due to the amount of greenhouse gases in the atmosphere.",
+		"image": "https://upload.wikimedia.org/wikipedia/commons/e/e5/Venus-real_color.jpg"
+	},
+	{
+		"id": 3,
+		"title": "Earth",
+		"text": "Earth is the largest and densest of the inner planets, the only one known to have current geological activity, and the only place where life is known to exist. Its liquid hydrosphere is unique among the terrestrial planets, and it is the only planet where plate tectonics has been observed. Earth's atmosphere is radically different from those of the other planets, having been altered by the presence of life to contain 21% free oxygen.[84] It has one natural satellite, the Moon, the only large satellite of a terrestrial planet in the Solar System.",
+		"image": "https://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg"
+	},
+	{
+		"id": 4,
+		"title": "Mars",
+		"text": "Mars is smaller than Earth and Venus. It has an atmosphere of mostly carbon dioxide with a surface pressure of 6.1 millibars. Its surface, peppered with vast volcanoes, such as Olympus Mons, and rift valleys, such as Valles Marineris, shows geological activity that may have persisted until as recently as 2 million years ago. Its red colour comes from iron oxide (rust) in its soil.",
+		"image": "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg"
+	},
+	{
+		"id": 5,
+		"title": "Jupiter",
+		"text": "Jupiter is 2.5 times the mass of all the other planets put together. It is composed largely of hydrogen and helium. Jupiter's strong internal heat creates semi-permanent features in its atmosphere, such as cloud bands and the Great Red Spot. Jupiter has 79 known satellites. The four largest, Ganymede, Callisto, Io, and Europa, show similarities to the terrestrial planets, such as volcanism and internal heating.",
+		"image": "https://upload.wikimedia.org/wikipedia/commons/5/50/Jupiter%2C_image_taken_by_NASA%27s_Hubble_Space_Telescope%2C_June_2019_-_Edited.jpg"
+	},
+	{
+		"id": 6,
+		"title": "Saturn",
+		"text": "Saturn, distinguished by its extensive ring system, has several similarities to Jupiter, such as its atmospheric composition and magnetosphere. Although Saturn has 60% of Jupiter's volume, it is less than a third as massive. Saturn is the only planet of the Solar System that is less dense than water.[100] The rings of Saturn are made up of small ice and rock particles.",
+		"image": "https://upload.wikimedia.org/wikipedia/commons/c/c7/Saturn_during_Equinox.jpg"
+	},
+	{
+		"id": 7,
+		"title": "Uranus",
+		"text": "Uranus is the lightest of the outer planets. Uniquely among the planets, it orbits the Sun on its side; its axial tilt is over ninety degrees to the ecliptic. It has a much colder core than the other giant planets and radiates very little heat into space. Uranus has 27 known satellites, the largest ones being Titania, Oberon, Umbriel, Ariel, and Miranda.",
+		"image": "https://upload.wikimedia.org/wikipedia/commons/3/3d/Uranus2.jpg"
+	},
+	{
+		"id": 8,
+		"title": "Neptune",
+		"text": "Neptune, though slightly smaller than Uranus, is more massive and hence more dense. It radiates more internal heat, but not as much as Jupiter or Saturn. Neptune has 14 known satellites. The largest, Triton, is geologically active, with geysers of liquid nitrogen.[104] Triton is the only large satellite with a retrograde orbit.",
+		"image": "https://upload.wikimedia.org/wikipedia/commons/5/56/Neptune_Full.jpg"
+	}
+];
